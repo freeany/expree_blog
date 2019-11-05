aa5b0021784365cb5be2eb1c31045eafc061e984
lhr <812974019@qq.com>
2019-11-05 09:22:29 +0800

    使用redis存储sessionId

--- db/redis.js
+++ db/redis.js
@@ -1,12 +1,12 @@
 /* 
     将redis做成工具
+    使用redis存储 session数据
     
 */
 const redis = require('redis')
 const {REDIS_CONF}  = require('../conf/db')
 
 // 创建redis客户端
- 
 const RedisClient =  redis.createClient(REDIS_CONF.port,REDIS_CONF.host)
 RedisClient.on('error', err => {
     console.error(err)
